474bc709aa7b8047a2a3ad56706b7b9661023f2c
Marin Bareta <mbareta@extensionengine.com>
2016-08-12 16:10:10 +0200

    Working state, still waiting for original pictures

--- community_eyes/community_eyes.py
+++ community_eyes/community_eyes.py
@@ -15,12 +15,6 @@ class CommunityEyesXBlock(XBlock):
     # Fields are defined on the class.  You can access them in your code as
     # self.<fieldname>.
 
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
-
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -35,22 +29,31 @@ class CommunityEyesXBlock(XBlock):
         html = self.resource_string("static/html/community_eyes.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/community_eyes.css"))
+        frag.add_css_url(self.runtime.local_resource_url(self, 'public/css/main.css'))
         frag.add_javascript(self.resource_string("static/js/src/community_eyes.js"))
         frag.initialize_js('CommunityEyesXBlock')
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
 
-        self.count += 1
-        return {"count": self.count}
+    def studio_view(self, context):
+        """
+        Create a fragment used to display the edit view in the Studio.
+        """
+        html_str = pkg_resources.resource_string(__name__, "static/html/studio_view.html")
+        frag = Fragment(unicode(html_str).format(display_name=self.display_name))
+        js_str = pkg_resources.resource_string(__name__, "static/js/src/studio_edit.js")
+        frag.add_javascript(unicode(js_str))
+        frag.initialize_js('StudioEdit')
+        return frag
+
+    @XBlock.json_handler
+    def studio_submit(self, data, suffix=''):
+        """
+        Called when submitting the form in Studio.
+        """
+        self.display_name = data.get('display_name')
+
+        return {'result': 'success'}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
