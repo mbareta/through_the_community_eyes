2b83564ed8f33c0669c716e5fe35a4ab7d9ec898
Marin Bareta <mbareta@extensionengine.com>
2016-11-08 09:51:20 +0100

    Update resize function for portrait and fix trigger placement

--- community_eyes/static/js/src/community_eyes.js
+++ community_eyes/static/js/src/community_eyes.js
@@ -22,11 +22,21 @@ function CommunityEyesXBlock(runtime, element) {
             var imageAspectRatio = 1.6;
             var aspectRatioDelta = (windowAspectRatio - imageAspectRatio)/imageAspectRatio;
 
-            var imageStretch = 100 + aspectRatioDelta*100;
+            if(windowAspectRatio >= imageAspectRatio) {
+                var imageStretch = 100 + aspectRatioDelta*100;
 
-            $('#container, .expander-image')
-                .css('background-size', '100% ' + imageStretch + '%')
-                .css('background-position', '0 ' + aspectRatioDelta*50 + '%');
+                $('#container, .expander-image')
+                    .css('background-size', '100% ' + imageStretch + '%')
+                    .css('background-position', '0 ' + aspectRatioDelta*50 + '%');
+            }
+            else {
+                var imageStretch = 100 - aspectRatioDelta*100;
+
+                $('#container, .expander-image')
+                    .css('background-size', imageStretch + '% 100%')
+                    .css('background-position', '-' + aspectRatioDelta*50 + '% 0');
+            }
+
 
         });
     });
