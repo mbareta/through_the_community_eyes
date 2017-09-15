3071ac062db47654021e5dad542b445757d163c4
Ivana Vukovic <ivana.st@windowslive.com>
2017-09-15 15:53:09 +0000

    extract css, add zoomed class when needed, remove <br> lines

--- community_eyes/static/js/src/community_eyes.js
+++ community_eyes/static/js/src/community_eyes.js
@@ -1,43 +1,59 @@
 /* Javascript for CommunityEyesXBlock. */
 function CommunityEyesXBlock(runtime, element) {
+    var $element = $(element);
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
+    applyClassIfZoomed($element, getZoomLevel());
+
+    // onResize event can also catch zoom
+    $(window).on('resize', function () {
+        applyClassIfZoomed($element, getZoomLevel());
+        var windowAspectRatio = this.innerWidth / this.innerHeight;
+        var imageAspectRatio = 1.6;
+        var aspectRatioDelta = (windowAspectRatio - imageAspectRatio) / imageAspectRatio;
+
+        if (windowAspectRatio >= imageAspectRatio) {
+            var imageStretch = 100 + aspectRatioDelta * 100;
+
+            $('#container, .expander-image')
+                .css('background-size', '100% ' + imageStretch + '%')
+                .css('background-position', '0 ' + aspectRatioDelta * 50 + '%');
+        }
+        else {
+            var imageStretch = 100 - aspectRatioDelta * 100;
+
+            $('#container, .expander-image')
+                .css('background-size', imageStretch + '% 100%')
+                .css('background-position', '-' + aspectRatioDelta * 50 + '% 0');
+        }
+
+
+    });
+
+    function getZoomLevel() {
+        var screenCssPixelRatio = (window.outerWidth - 8) / window.innerWidth;
+
+        if (screenCssPixelRatio <= 1.10) {
+            return 0;
+        } else if (screenCssPixelRatio <= 1.32) {
+            return 1;
+        } else if (screenCssPixelRatio <= 1.58) {
+            return 2;
+        } else if (screenCssPixelRatio <= 1.90) {
+            return 3;
+        } else if (screenCssPixelRatio <= 2.28) {
+            return 4;
+        } else if (screenCssPixelRatio <= 2.70) {
+            return 5;
+        } else {
+            return undefined;
+        }
     }
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
-
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
-    });
-
-    $(function ($) {
-        $(window).on('resize', function(){
-            var windowAspectRatio = this.innerWidth/this.innerHeight;
-            var imageAspectRatio = 1.6;
-            var aspectRatioDelta = (windowAspectRatio - imageAspectRatio)/imageAspectRatio;
-
-            if(windowAspectRatio >= imageAspectRatio) {
-                var imageStretch = 100 + aspectRatioDelta*100;
-
-                $('#container, .expander-image')
-                    .css('background-size', '100% ' + imageStretch + '%')
-                    .css('background-position', '0 ' + aspectRatioDelta*50 + '%');
-            }
-            else {
-                var imageStretch = 100 - aspectRatioDelta*100;
-
-                $('#container, .expander-image')
-                    .css('background-size', imageStretch + '% 100%')
-                    .css('background-position', '-' + aspectRatioDelta*50 + '% 0');
-            }
-
-
-        });
-    });
+    function applyClassIfZoomed($el, zoomLevel) {
+        if(zoomLevel >= 2) {
+            $el.addClass('zoomed');
+        } else {
+            $el.removeClass('zoomed');
+        }
+    }
 }
