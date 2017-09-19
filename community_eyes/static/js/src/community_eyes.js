1ce545eb6d6fd56de21058c72951a1f1a7dc6c26
Ivana Vukovic <ivana.st@windowslive.com>
2017-09-19 06:50:29 +0000

    remove zoom function

--- community_eyes/static/js/src/community_eyes.js
+++ community_eyes/static/js/src/community_eyes.js
@@ -1,12 +1,7 @@
 /* Javascript for CommunityEyesXBlock. */
 function CommunityEyesXBlock(runtime, element) {
-    var $element = $(element);
-
-    applyClassIfZoomed($element, getZoomLevel());
-
-    // onResize event can also catch zoom
+    
     $(window).on('resize', function () {
-        applyClassIfZoomed($element, getZoomLevel());
         var windowAspectRatio = this.innerWidth / this.innerHeight;
         var imageAspectRatio = 1.6;
         var aspectRatioDelta = (windowAspectRatio - imageAspectRatio) / imageAspectRatio;
@@ -28,28 +23,4 @@ function CommunityEyesXBlock(runtime, element) {
 
 
     });
-
-    function getZoomLevel() {
-        var screenCssPixelRatio = (window.outerWidth - 8) / window.innerWidth;
-
-        if (screenCssPixelRatio <= 1.10) {
-            return 0;
-        } else if (screenCssPixelRatio <= 1.32) {
-            return 1;
-        } else if (screenCssPixelRatio <= 1.58) {
-            return 2;
-        } else if (screenCssPixelRatio <= 1.90) {
-            return 3;
-        } else if (screenCssPixelRatio <= 2.28) {
-            return 4;
-        } else if (screenCssPixelRatio <= 2.70) {
-            return 5;
-        } else {
-            return undefined;
-        }
-    }
-
-    function applyClassIfZoomed($el, zoomLevel) {
-        zoomLevel >= 2 ? $el.addClass('zoomed') : $el.removeClass('zoomed');
-    }
 }
